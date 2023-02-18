668ed93629743a22b7341d4800f481a9d501671e
szjozi <szjozefczuk.sda@gmail.com>
2023-02-18 12:56:37 +0100

    UsersController findByUsername added

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,17 +1,24 @@
 package com.sda;
 
+import com.sda.controller.UsersController;
 import com.sda.dao.UsersDAO;
+import com.sda.mapper.UserMapper;
 import com.sda.model.User;
+import com.sda.service.UsersService;
 
 public class Main {
 
     public static void main(String[] args) {
         UsersDAO usersDAO = new UsersDAO();
+        UserMapper userMapper = new UserMapper();
+        UsersService usersService = new UsersService(usersDAO, userMapper);
+        UsersController usersController = new UsersController(usersService);
 
         User user = new User();
-        user.setUsername("root");
+        user.setUsername("root1");
+//        usersService.create(user);
 
-        usersDAO.create(user);
+        usersController.findByUsername("root2");
 
     }
 
---
+++ src/main/java/com/sda/controller/UsersController.java
@@ -0,0 +1,43 @@
+package com.sda.controller;
+
+import com.sda.dto.UserDTO;
+import com.sda.exception.NotFoundException;
+import com.sda.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+
+@Slf4j
+@RequiredArgsConstructor
+public class UsersController {
+
+//    private static final Logger log = LoggerFactory.getLogger(UsersController.class);
+
+    private final UsersService usersService;
+
+    public void findAll() {
+        List<UserDTO> users = usersService.findAll();
+
+        if (users.isEmpty()) {
+            System.out.println("Users list empty!");
+        } else {
+            System.out.println("Users list:");
+            users.forEach(user -> System.out.println(user));
+//            users.forEach(System.out::println);
+        }
+    }
+
+    public void findByUsername(String username) {
+        try {
+            UserDTO userDTO = usersService.findByUsername(username);
+            System.out.printf("User found: %s%n", userDTO);
+        } catch (NotFoundException ex) {
+            log.error("NotFoundException: {}", ex.getMessage());
+        }
+    }
+
+}
